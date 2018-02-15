8a99b2c8c08eaf2c804621bfdf2be6f4192fda52
mordohaw <william.mordohay@gmail.com>
2018-02-15 17:50:58 +0100

    add new firebase dependancies

--- app/src/main/java/fr/citizenfood/citizenfood/LoginActivity.java
+++ app/src/main/java/fr/citizenfood/citizenfood/LoginActivity.java
@@ -3,8 +3,14 @@ package fr.citizenfood.citizenfood;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
