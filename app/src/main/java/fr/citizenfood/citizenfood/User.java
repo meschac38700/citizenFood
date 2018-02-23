4641b3ed5f8cb9698a96ea0357a33365e57efd0e
mordohaw <william.mordohay@gmail.com>
2018-02-23 17:50:56 +0100

    add signUp

---
+++ app/src/main/java/fr/citizenfood/citizenfood/User.java
@@ -0,0 +1,21 @@
+package fr.citizenfood.citizenfood;
+
+/**
+ * Created by William on 23/02/2018.
+ */
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
